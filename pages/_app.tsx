d27aef9f82d6d5466ac8ba7fe3642bf9510984fb
Devinda Senanayaka <dsenanayaka1@gmail.com>
2021-01-19 02:45:29 -0600

    added input with custom styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,25 @@ import '../styles/globals.css'
 import { Fonts } from "../theme/fonts"
 
 const theme = extendTheme({
+  components:{
+    Input: {
+      baseStyle: {
+        fontFamily: "Cinzel",
+        fontSize: "36px",
+        bg: "#E7E7E7",
+        borderColor: "#5270a4",
+        borderRadius: "0",
+        textAlign: "center",
+        color: "#5270a4",
+        border: "1px solid",
+        h: "97px",
+        w: "336px",
+        "&::placeholder": {
+          color: "rgba(82, 112, 164, 0.65)"
+        },
+      }
+    }
+  },
   fonts: {
     heading: "Cinzel Decorative",
     body: "Nunito"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import { Flex, Square, Center, Text, Box, Heading } from "@chakra-ui/react"
+// import {Input} from '@chakra-ui/react'
+import { useStyleConfig } from "@chakra-ui/react"
+
+function Input(props){
+  const { size, variant, ...rest } = props
+  // 2. Reference `Button` stored in `theme.components`
+  const styles = useStyleConfig("Input", { size, variant })
+  // 3. Pass the computed styles into the `sx` prop
+  return <Box as="input" sx={styles} {...rest} />
+}
 
 export default function Home() {
   return (
-   <Box w="80%" h="80vh" bg="brand.white" borderRadius="30px">
+    <Flex w="80%" h="80vh" bg="brand.white" borderRadius="30px" flexDirection="column" alignContent="flex-end">
       <Heading color="brand.200" fontSize="64px" textAlign="center" mt={28}> Dota 2 Drafter </Heading>
-   </Box>
+      <Flex flexDirection="column" justify="space-between" h="500px">
+        <Input m="auto" placeholder="Username" />
+        <Input m="auto" placeholder="Username" />
+        <Input m="auto" placeholder="Username" />
+      </Flex>
+   </Flex>
   )
 }
